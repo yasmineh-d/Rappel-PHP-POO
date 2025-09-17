2284ebb8e902d7f432d86f0b485e7e0d2d201449
yasmineh-d <yassminehadad75@gmail.com>
2025-09-17 09:52:44 +0100

    fonction_tableaux is done

--- fonctions_tableaux/fonction_typées.php
+++ fonctions_tableaux/fonction_typées.php
@@ -8,6 +8,5 @@ function prixTTC(float $ht, float $taux = 0.2): float {
 function somme(int ...$nums): int { // variadics
     return array_sum($nums);
 }
-
 echo prixTTC(100.0); // 120
 echo somme(1,2,3,4); // 10
